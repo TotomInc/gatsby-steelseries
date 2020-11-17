13e9307821084761d8bcd305b14f43b9d53bc404
Thomas Cazade <cazade.thomas@gmail.com>
2020-11-17 23:15:41 +0100

    Clean boilerplate, prepare for Prismic CMS

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+
+  extends: '@totominc/react',
+
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'camelcase': 'off',
+    'no-underscore-dangle': 'off',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+const prettierrc = require('@totominc/eslint-config-react/.prettierrc');
+
+module.exports = prettierrc;
