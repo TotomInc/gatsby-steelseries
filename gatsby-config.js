b710c016b80ecfae383526d19d696394de173c7b
Thomas Cazade <cazade.thomas@gmail.com>
2020-11-18 20:38:32 +0100

    Add tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-emotion`,
+    `gatsby-plugin-postcss`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.html'],
+  darkMode: true,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
