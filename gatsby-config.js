231289c7474f58cd536e87c165ece4b548e3d894
Thomas Cazade <cazade.thomas@gmail.com>
2020-11-22 22:02:21 +0100

    Add homepage prismic.io schema

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         shouldDownloadImage: () => true,
         schemas: {
           product: require('./schemas/product.json'),
+          homepage: require('./schemas/homepage.json'),
         },
       },
     },
