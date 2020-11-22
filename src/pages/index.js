096f3078f67c04850244477cde2104e93a1730cf
Thomas Cazade <cazade.thomas@gmail.com>
2020-11-22 17:23:40 +0100

    Add SEO to index with default share-image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 import { Card } from '../components/product/Card';
+import { SEO } from '../components/SEO';
 
 export const query = graphql`
   {
@@ -50,15 +51,26 @@ const IndexPage = ({ data: { allPrismicProduct } }) => {
     }));
 
   return (
-    <div className="p-8">
-      <h1 className="font-replica-pro font-black text-4xl mb-8">All our products</h1>
+    <>
+      <SEO
+        pageTitle="SteelSeries Replica Website | Made with Gatsby &amp; Prismic.io"
+        pageDescription="A SteelSeries e-commerce replica website made with Gatsby (React SSR) &amp; Prismic.io (headless CMS) as a learning and educational project."
+        pageUrl="https://steelseries.totominc.io"
+        shareTitle="SteelSeries Replica Website | Made with Gatsby &amp; Prismic.io"
+        shareDescription="A SteelSeries e-commerce replica website made with Gatsby (React SSR) &amp; Prismic.io (headless CMS) as a learning and educational project."
+        shareImage="https://steelseries.totominc.io/static/default_share_image.png"
+      />
 
-      <div className="flex flex-wrap max-w-screen-2xl 2xl:mx-auto">
-        {products.map((product) => (
-          <Card product={product} key={product.id} />
-        ))}
+      <div className="p-8">
+        <h1 className="font-replica-pro font-black text-4xl mb-8">All our products</h1>
+
+        <div className="flex flex-wrap max-w-screen-2xl 2xl:mx-auto">
+          {products.map((product) => (
+            <Card product={product} key={product.id} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
