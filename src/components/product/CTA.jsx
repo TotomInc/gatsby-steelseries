7719c778f0e2717f7274113d4d8c31fe9b42d28a
Thomas Cazade <cazade.thomas@gmail.com>
2020-11-22 15:50:45 +0100

    CTA hover bg-transition

--- src/components/product/CTA.jsx
+++ src/components/product/CTA.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CTA = ({ className }) => (
   <button
-    className={`w-full px-8 py-4 rounded-sm text-white font-bold text-xl bg-steelseries-orange ${className}`}
+    className={`w-full px-8 py-4 rounded-sm text-white font-bold text-xl transition-all ease-in-out duration-150 bg-steelseries-orange hover:bg-opacity-80 ${className}`}
     type="button"
   >
     Add to cart
