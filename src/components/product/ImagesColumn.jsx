384ee0020421a888360020e07b23459e5d61870a
Thomas Cazade <cazade.thomas@gmail.com>
2020-11-22 15:56:02 +0100

    Hover border on product images

--- src/components/product/ImagesColumn.jsx
+++ src/components/product/ImagesColumn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ImagesColumn = ({ images }) => (
   <section className="flex mb-4">
-    <div className="w-1/2 mr-2 bg-gray-200 rounded-sm">
+    <div className="w-1/2 mr-2 bg-gray-200 rounded-sm border-2 border-transparent hover:border-black">
       <img
         className="block object-contain rounded-sm"
         src={images.image_left.localFile.childImageSharp.original.src}
@@ -10,7 +10,7 @@ const ImagesColumn = ({ images }) => (
       />
     </div>
 
-    <div className="w-1/2 ml-2 bg-gray-200 rounded-sm">
+    <div className="w-1/2 ml-2 bg-gray-200 rounded-sm border-2 border-transparent hover:border-black">
       <img
         className="block object-contain rounded-sm"
         src={images.image_right.localFile.childImageSharp.original.src}
--- src/components/product/LargeImage.jsx
+++ src/components/product/LargeImage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LargeImage = ({ image }) => (
   <section className="flex mb-4">
-    <div className="w-full bg-gray-200 rounded-sm">
+    <div className="w-full bg-gray-200 rounded-sm border-2 border-transparent hover:border-black">
       <img
         className="block object-contain rounded-sm"
         src={image.large_image.localFile.childImageSharp.original.src}
